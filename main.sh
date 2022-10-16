bb7f8847556f7369bb03e2293e2ca6b42bde4bf1
dao2 <tangziyang666@gmail.com>
2022-10-15 23:22:56 -0400

    add bash for real data

--- main.sh
+++ main.sh
@@ -4,17 +4,17 @@ for K in 2
 do
     for p in $(seq 0.01 0.01 0.02)
     do
-	for alpha in $(seq 0.1 0.1 0.2)
-	do
-	    cd src
-	    Rscript spaCI_preprocess.R ../example_data/st_expression.csv ../example_data/st_meta.csv ${K} ${p}  '../data_IO'
-	    cd ..
-	    # # sed 's/^\(THRESHOLD: \).*$/\1'"$thr"'/' configure.yml
-	    # # cp $yml configure.yml
-	    python3 find_best_parameters.py --alpha alpha --k K --p p
-	done
+        for alpha in $(seq 0.8 0.1 0.9)
+        do
+            # echo "${K}, ${p}, ${alpha}"
+            cd src
+            /opt/R/4.0.2/bin/Rscript spaCI_preprocess.R ../example_data/st_expression.csv ../example_data/st_meta.csv ${K} ${p}  '../data_IO/real_data'
+            cd ..
+            python3 find_best_parameters.py --alpha ${alpha} --k ${K} --p ${p}
+        done
     done
 done
    
 # print log name
+python3 print_best_parameters.py
 
