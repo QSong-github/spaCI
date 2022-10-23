9c6b9c577dae7ba81c8cde0aac992cb5447cd626
dao2 <tangziyang666@gmail.com>
2022-10-23 17:51:13 -0400

    add r src

--- src/spaCI_preprocess.R
+++ src/spaCI_preprocess.R
@@ -6,12 +6,14 @@ st.file = args[[1]]
 meta.file  = args[[2]]
 k=as.numeric(args[[3]])
 cut=as.numeric(args[[4]])
-dir0=args[[5]]
+database = args[[5]]
+dir0=args[[6]]
 
 st.exp <- read.csv(file=st.file,stringsAsFactors=F,check.names=F,row.names=1)
 st.meta <- read.csv(file=meta.file,stringsAsFactors=F,check.names=F,row.names=1)                    
 source('spaCI_utils.R')
-LRdB <- readRDS('L-R database.RDS')
+
+LRdB <- readRDS(database)
 
 spaCI_preprocess(st.exp = st.exp,
                  st.meta = st.meta,
