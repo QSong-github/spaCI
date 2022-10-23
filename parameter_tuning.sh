207194acb3fe8094fe5b25746ca70908f50a8f7f
dao2 <tangziyang666@gmail.com>
2022-10-23 17:51:58 -0400

    add parameter tuning

--- parameter_tuning.sh
+++ parameter_tuning.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
-for K in 2..8
+for K in $(seq 2 1 8)
 do
     for p in $(seq 0.01 0.01 0.1)
     do
         for alpha in $(seq 0.1 0.1 0.9)
         do
             cd src
-            /opt/R/4.0.2/bin/Rscript spaCI_preprocess.R ../dataset/example_data/st_expression.csv ../dataset/example_data/st_meta.csv ${K} ${p}  '../dataset/real_data'
+            /opt/R/4.0.2/bin/Rscript spaCI_preprocess.R "../dataset/example_data/st_expression.csv"  "../dataset/example_data/st_meta.csv" ${K} ${p} 'spaCI_database.RDS' '../dataset/real_data'
             cd ..
             python optimize_parameter.py --alpha ${alpha} --k ${K} --p ${p}
         done
