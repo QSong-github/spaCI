da03243eac88b792310f5f590f7f612c25a83467
Qianqian Song <wasqqdyx@gmail.com>
2022-10-23 20:45:23 -0400

    Update parameter_tuning.sh

--- parameter_tuning.sh
+++ parameter_tuning.sh
@@ -7,7 +7,7 @@ do
         for alpha in $(seq 0.1 0.1 0.9)
         do
             cd src
-            /opt/R/4.0.2/bin/Rscript spaCI_preprocess.R "../dataset/example_data/st_expression.csv"  "../dataset/example_data/st_meta.csv" ${K} ${p} 'spaCI_database.RDS' '../dataset/real_data'
+            /path/to/Rscript spaCI_preprocess.R "../dataset/example_data/st_expression.csv"  "../dataset/example_data/st_meta.csv" ${K} ${p} 'spaCI_database.RDS' '../dataset/real_data'
             cd ..
             python optimize_parameter.py --alpha ${alpha} --k ${K} --p ${p}
         done
